cc15da8218733065dbf40b7729ceaff3625c6328
OlhaDudchenko <dudchenko.olha92@gmail.com>
2021-07-13 21:01:43 +0300

    add library

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import './css/styles.css';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
 import { fetchCountries } from "./js/fetchCountries";
@@ -10,3 +10,22 @@ import "./css/styles.css";
 //     fetch(`${BASE_URL}/rest/v2/name/${name}`).then(response => { return response.json() }).then(country => { console.log(country) }).catch(error => { console.log(error)});
 // }
 // fetchCountries('sweden');
+const input = document.querySelector('[id="search-box"]');
+console.log(input);
+const countryList = document.querySelector(".country-list");
+console.log(countryList);
+
+input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+ export function onInput(evt) {
+    console.log(evt.target.value)
+    const name = evt.target.value;
+    fetchCountries(name);
+    countryList.textContent = name;
+
+
+
+}
+
+
+// console.log(name);
