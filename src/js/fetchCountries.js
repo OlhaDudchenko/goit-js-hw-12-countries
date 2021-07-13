6d27acc01be2498b157fb5fdc1a75a342d1f458a
OlhaDudchenko <dudchenko.olha92@gmail.com>
2021-07-13 21:02:06 +0300

    add code

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,5 +1,25 @@
+
+
+
 export function fetchCountries(name) {
     const BASE_URL ='https://restcountries.eu';
-    fetch(`${BASE_URL}/rest/v2/name/${name}`).then(data => { console.log(data)});
+    fetch(`${BASE_URL}/rest/v2/name/${name}`).then(response=>response.json().name).then(data => {
+  console.log(data)
+  })
+  .catch(error => {
+  console.log(error)
+  });
 }
-  fetchCountries('sweden')
+  
+// function filterResponse(service,fields) {
+//   // https://restcountries.eu/rest/v2/{service}?fields={field};{field};{field}
+//   // https://restcountries.eu/rest/v2/all?fields=name;capital;currencies
+//   const fields = [name,capital,population,flag,languages]
+//   const service = all;
+//   fetch(`${BASE_URL}/rest/v2/name/${service}?${fields}=${field};${field};${field}`).then(data => {
+//   console.log(data)
+//   })
+//   .catch(error => {
+//   console.log(error)
+//   });
+// }
