328a131b7a6e333ee951496457b2c8c49b62524f
OlhaDudchenko <dudchenko.olha92@gmail.com>
2021-07-14 21:48:47 +0300

    function fetchCountries

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,24 +2,18 @@
 
 
 export function fetchCountries(name) {
-    const BASE_URL ='https://restcountries.eu';
-    fetch(`${BASE_URL}/rest/v2/name/${name}`).then(response=>response.json().name).then(data => {
-  console.log(data)
-  })
-  .catch(error => {
-  console.log(error)
-  });
-}
+  const BASE_URL = 'https://restcountries.eu';
   
-// function filterResponse(service,fields) {
-//   // https://restcountries.eu/rest/v2/{service}?fields={field};{field};{field}
-//   // https://restcountries.eu/rest/v2/all?fields=name;capital;currencies
-//   const fields = [name,capital,population,flag,languages]
-//   const service = all;
-//   fetch(`${BASE_URL}/rest/v2/name/${service}?${fields}=${field};${field};${field}`).then(data => {
-//   console.log(data)
-//   })
-//   .catch(error => {
-//   console.log(error)
-//   });
-// }
+  fetch(`${BASE_URL}/rest/v2/name/${name}?fields=name;capital;population;flag;languages`).then(response =>
+  {if (!response.ok) {
+      throw new Error(response.status);
+  }
+    return response.json()
+  }).then(data => {
+    console.log(data)
+  })
+    .catch(error => {
+      console.log("Error : ", error)
+    });
+ 
+}
